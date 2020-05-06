685eac85ec5e335c463445bf88632ce92c87615f
Julian Gutierrez <juliansouza@gmail.com>
2020-05-06 07:45:29 -0300

    Configurando exibição de casas decimais

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
 	def big_decimal_to_currency value, precision
 		number_to_currency value, precision: precision, unit: ''
 	end
+
+	def big_decimal_to_currency_cents value
+		big_decimal_to_currency value, 2
+	end
 end
