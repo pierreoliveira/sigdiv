e44c7fc56dff7d10eb5787e0dcff567e2fe582e9
Julian Gutierrez <juliansouza@gmail.com>
2020-06-05 17:37:33 -0300

    Implementando fórmula

--- app/controllers/formulas_controller.rb
+++ app/controllers/formulas_controller.rb
@@ -1,5 +1,8 @@
 class FormulasController < ApplicationController
 	def show
-		render json: params[:formula] + 'ok'
+		#render json: params[:formula] + 'ok'
+		respond_to do |format|
+		  format.json { render json: { response: Dentaku(params[:formula]) } } 
+		end
 	end
 end
